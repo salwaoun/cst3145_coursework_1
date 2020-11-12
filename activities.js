827b0fe7dc6e8bd5ee2453b576ee1e514839c27c
Salwa Abo Oun <salwaoun8@gmail.com>
2020-11-12 17:34:51 +0400

    Initial code

---
+++ activities.js
@@ -0,0 +1,29 @@
+let activities=[
+  {
+    id: 0001,
+    title: "Gymnastics",
+    location: "UAE",
+    price: 900,
+    spaces: 5,
+    image: "images/gymnastics.png",
+    rating: 3,
+  },
+  {
+    id: 0002,
+    title: "Dance",
+    location: "UAE",
+    price: 1000,
+    spaces: 5,
+    image: "images/dance.png",
+    rating: 2,
+  },
+  {
+    id: 0003,
+    title: "Cooking",
+    location: "UAE",
+    price: 500,
+    spaces: 5,
+    image: "images/cooking.png",
+    rating: 4,
+    }
+]
